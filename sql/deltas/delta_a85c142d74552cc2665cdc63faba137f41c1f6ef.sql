414212c7f977dc5191fbf2b35c9e1fe17df13adc
Paul Yushkevich <pyushkevich@gmail.com>
2018-04-19 18:29:45 +0000

    sql fix

--- sql/deltas/delta_a85c142d74552cc2665cdc63faba137f41c1f6ef.sql
+++ sql/deltas/delta_a85c142d74552cc2665cdc63faba137f41c1f6ef.sql
@@ -2,7 +2,7 @@
 create or replace view success_ticket_duration as 
   select T.service_githash, T1.ticket_id, T1.atime as endtime, T1.atime - max(T2.atime) as runtime 
   from ticket_history T1, ticket_history T2, tickets T where T1.status='success' 
-       and T1.ticket_id = T2.ticket_id and T2.status='claimed' and T.id = T1.ticket_id 
+       and T1.ticket_id = T2.ticket_id and T2.status='claimed' and T.id = T1.ticket_id;
 
 /* Add pingtime to services */
 ALTER TABLE services ADD COLUMN pingtime timestamp not null default current_timestamp;
